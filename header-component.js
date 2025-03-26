ef0eadb55d5fcc9f74585324c99fa6a33b998276
Fikri Ghazi <fikrigha@gmail.com>
2025-03-26 12:13:18 -0400

    update

--- header-component.js
+++ header-component.js
@@ -4,18 +4,18 @@ class CustomHeader extends HTMLElement {
         <header>
             <a href="/"><h1>Fikri Ghazi</h1></a>
             <nav>
-                <a href="mailto:fikri@mghazi.com">
-                  <small>Email</small>
-                </a>
-                <a href="https://www.linkedin.com/in/fikrigha/" target="_blank">
-                  <small>LinkedIn</small>
-                </a>
-                <a href="https://github.com/fikrigha" target="_blank">
-                  <small>GitHub</small>
-                </a>
-                <a href="https://twitter.com/fikrigha" target="_blank">
-                  <small>Twitter</small>
-                </a>
+              <a href="mailto:fikri@mghazi.com">
+                <small>Email</small>
+              </a>
+              <a href="https://www.linkedin.com/in/fikrigha/" target="_blank">
+                <small>LinkedIn</small>
+              </a>
+              <a href="https://github.com/fikrigha" target="_blank">
+                <small>GitHub</small>
+              </a>
+              <a href="https://twitter.com/fikrigha" target="_blank">
+                <small>Twitter</small>
+              </a>
             </nav>
         </header>
     `;
